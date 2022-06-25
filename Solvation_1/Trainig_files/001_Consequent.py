63f478ef3513a361835d55259fe0c45af634173c
Balepka212 <balepka212@gmail.com>
2022-06-25 12:37:10 +0300

    clear directory

--- Solvation_1/Trainig_files/001_Consequent.py
+++
@@ -1,9 +0,0 @@
-print('starting Blank Class Res')
-import Solvation_1.Trainig_files.Blank_Class_Res1
-print('finished Blank Class Res')
-print('starting Blank TESA Res')
-import Solvation_1.Trainig_files.Blank_TESA_Res1
-print('finished Blank TESA Res')
-print('starting Blank Morgan Res')
-import Solvation_1.Trainig_files.Blank_Morgan_Res1
-print('starting Blank Morgan Res')
