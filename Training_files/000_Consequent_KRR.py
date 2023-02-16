b8494df10c7f48ffc55861617fba3bfb1a0e44f6
balepka212 <balepka212@gmail.com>
2023-02-16 12:55:22 +0300

    upd KRR training

--- Training_files/000_Consequent_KRR.py
+++ Training_files/000_Consequent_KRR.py
@@ -8,26 +8,12 @@ def what_time():
     current_time = now.strftime("%H:%M:%S")
     print(current_time)
 
-# solvents = ('blank', 'class', 'macro', 'morgan', 'morgan2to20', 'jb', 'bob', 'bat', 'soap')
-# # solutes = ('blank', 'class', 'tesa', 'morgan', 'morgan2to20', 'jb', 'bob', 'bat', 'soap')
-# solutes = ('computedprops',)
-exclude = ('blankblank', 'morgan2to20morgan2to20', 'morgan2to20computedprops', )
-# for solvent in (solvents):
-#     for solute in (solutes):
-#         if solvent+solute not in exclude:
-#             what_time()
-#             KRR_experiment(solvent, solute, number=2, n_jobs=None)
+exclude = ('blankblank',)
 
-# solvents = ('blank', 'class', 'macro', 'morgan', 'morgan2to20', 'jb', 'bob', 'bat', 'soap')
-solutes = ('blank', 'class', 'tesa', 'computedprops', 'morgan', 'morgan2to20', 'jb', 'bob', 'bat', 'soap')
-solvents = ('morgan2to20',)
+solvents = ('blank', 'class', 'macro', 'macrox', 'morgan', 'morgan2to20', 'jb', 'bob', 'bat', 'soap')
+solutes = ('blank', 'class', 'tesa', 'computedprops', 'morgan', 'morgan2to20', 'jb', 'bob',  'bat', 'soap')
 for solvent in (solvents):
     for solute in (solutes):
         if solvent+solute not in exclude:
             what_time()
-            KRR_experiment(solvent, solute, number=2, n_jobs=None)
-
-# KRR_experiment('bat', 'soap', number=1)
-# BAT BAT
-# BAT SOAP
-# solutes = ('blank', 'class', 'tesa', 'morgan', 'jb', 'bob', 'bat', 'soap')
+            KRR_experiment(solvent, solute, number=1, n_jobs=None)
